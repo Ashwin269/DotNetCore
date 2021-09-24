ea13c9a61167ba5f40a8034fa269d7ed8d14491a
Ashwin269 <ashwineee91@gmail.com>
2021-09-24 19:29:25 +0200

    Sample Project

---
+++ DotNetCore/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotNetCore.Models
+{
+    public class Person
+    {
+        public readonly int personId;
+        public string firstName;
+        public string lastName;
+
+        public Person(int personId, string firstName, string lastName)
+        {
+            if(string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+                throw new ArgumentNullException("firstName, lastName", "firstName or lastName cannot be null or empty");
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+}
