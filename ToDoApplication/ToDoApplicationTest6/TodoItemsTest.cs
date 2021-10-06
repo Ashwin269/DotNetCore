efce2ee88b541461116dfada36fb9ac05519c321
Ashwin269 <ashwineee91@gmail.com>
2021-10-06 21:08:22 +0200

    added a file

--- ToDoApplication/ToDoApplicationTest6/TodoItemsTest.cs
+++ ToDoApplication/ToDoApplicationTest6/TodoItemsTest.cs
@@ -12,7 +12,7 @@ namespace ToDoApplicationTest6
             TodoItems todo = new TodoItems();
             todo.UpdateTodoArray("Son", null, false);
             todo.UpdateTodoArray("Daughter", new Person(PersonSequencer.nextPersonId(), "Vanshi", "Madhu"), true);
-            Assert.Equal("Son", TodoItems.TodoArray[0].description );
+            Assert.Equal("Son", TodoItems.TodoArray[0].description);
             Assert.Equal("Daughter", TodoItems.TodoArray[1].description);
             Assert.Equal("VanshiMadhu", TodoItems.TodoArray[1].assignee.firstName + TodoItems.TodoArray[1].assignee.lastName);
         }
@@ -42,8 +42,7 @@ namespace ToDoApplicationTest6
             TodoItems todo = new TodoItems();
             todo.UpdateTodoArray("Son", null, false);
             todo.UpdateTodoArray("Daughter", new Person(PersonSequencer.nextPersonId(), "Vanshi", "Madhu"), true);
-            Todo[] todoItem = new Todo[]{};
-            todoItem = todo.FindByAssignee(2);
+            var todoItem = todo.FindByAssignee(2);
             Assert.Equal("VanshiMadhu", todoItem[0].assignee.firstName + todoItem[0].assignee.lastName);
         }
 
@@ -53,9 +52,27 @@ namespace ToDoApplicationTest6
             TodoItems todo = new TodoItems();
             todo.UpdateTodoArray("Son", null, false);
             todo.UpdateTodoArray("Daughter", new Person(PersonSequencer.nextPersonId(), "Vanshi", "Madhu"), true);
-            Todo[] todoItem = new Todo[] { };
-            todoItem = todo.FindByAssignee(new Person(1, "Vanshi", "Madhu"));
+            var todoItem = todo.FindByAssignee(new Person(1, "Vanshi", "Madhu"));
             Assert.Equal("VanshiMadhu", todoItem[0].assignee.firstName + todoItem[0].assignee.lastName);
+        }
+        [Fact]
+        public void FindTodoByUnAssignedPerson()
+        {
+            TodoItems todo = new TodoItems();
+            todo.UpdateTodoArray("Son", null, false);
+            todo.UpdateTodoArray("Daughter", new Person(PersonSequencer.nextPersonId(), "Vanshi", "Madhu"), true);
+            Todo[] todoItem = new Todo[] { };
+            todoItem = todo.FindUnassignedTodoItems();
+            Assert.Equal(null, todoItem[0].assignee);
+        }
+        [Fact]
+        public void RemoveOneObjectfromTodoArray()
+        {
+            TodoItems todo = new TodoItems();
+            todo.UpdateTodoArray("Son", null, false);
+            todo.UpdateTodoArray("Daughter", new Person(PersonSequencer.nextPersonId(), "Vanshi", "Madhu"), true);
+            todo.RemoveObjectFromTodoArray(1);
+            Assert.Equal(1, TodoItems.TodoArray.Length);
         }
 
         [Fact]
